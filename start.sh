11053216dad613f30b4870f75ffc24373b6638a9
root <root@ecollection2.i1b3ilqbo2xuppajz4elxyylzg.ax.internal.cloudapp.net>
2018-08-01 05:12:32 +0000

    test

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 echo "--- GENERATING ---"
 cd /opt/generator
-./generate.sh protos config/config.json github.com/sriram323/Gateway
+./generate.sh protos config/config.json github.com/sriram323/micro
 
 echo "--- RUNNING ---"
-#cd /go/src/github.com/sriram323/Gateway/
-#go build grpc-gateway.go
-#go run grpc-gateway.go
+cd /go/src/github.com/sriram323/Gateway/
+go build grpc-gateway.go
+go run grpc-gateway.go
